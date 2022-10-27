f2c480e776be5fed11d64193cbad7593b0e2e297
theo-beep <56303873+theo-beep@users.noreply.github.com>
2022-10-27 10:07:46 +0200

    - screen

---
+++ app/src/main/java/com/theolin/mvvmcleanarchitectureapp/presentation/coinlist/composable/CoinListScreen.kt
@@ -0,0 +1,30 @@
+package com.theolin.mvvmcleanarchitectureapp.presentation.coinlist.composable
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.theolin.mvvmcleanarchitectureapp.presentation.coinlist.composable.component.CoinListItem
+
+@Composable
+fun CoinListScreen(
+    navController: NavController,
+    viewModel: CoinListViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(state.coins) { coin ->
+                CoinListItem(
+                    coin = coin,
+                    onItemClick = {})
+
+            }
+        }
+    }
+
+}
